fa6bb9deee98be832251b84c316a9d802c95c59a
asdf <jflatley2016@gmail.com>
2026-01-04 05:03:05 -0600

    git add . git commit -m "Flatten project structure – moved FastAPI app files to repo root for Render deployment" git push origin master

---
+++ High_Probability_Model_v17/__init__.py
@@ -0,0 +1,5 @@
+# High_Probability_Model_v17/__init__.py
+from .build_live_context import build_live_context
+from .validate_gates import validate_gates
+
+__all__ = ["build_live_context", "validate_gates"]
--- hpb-tct-server/High_Probability_Model_v17/__init__.py
+++
@@ -1,5 +0,0 @@
-# High_Probability_Model_v17/__init__.py
-from .build_live_context import build_live_context
-from .validate_gates import validate_gates
-
-__all__ = ["build_live_context", "validate_gates"]
